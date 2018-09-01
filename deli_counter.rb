3f004304a7dbe759850991355c2e569642b13187
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 20:31:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+katz_deli = ["Logan", "Avi", "Spencer"]
+def line(katz_deli)
+  if katz_deli.count >= 1
+    katz_deli.each_with_index do |name, index|
+      print "#{index}. #{name} "
+
+    end
+
+  else
+    puts "The line is currently empty."
+  end
+end
+binding.pry
+katz_deli.unshift("The line is currently: ")
