ad5043793507bdec631b43f5f0b02637e4b9a05a
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 21:27:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.count >= 1
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  else
+    "There is nobody waiting to be served!"
+  end
+end
