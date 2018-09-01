a032d8a3f73bf8be6881862cfa55bcf16b12b3d2
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 20:51:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ katz_deli = ["Logan", "Avi", "Spencer"]
 def line(katz_deli)
   if katz_deli.count >= 1
     katz_deli.each_with_index do |name, index|
-      print "The line is currently: "
       print " #{index + 1}. #{name} "
     end
 
