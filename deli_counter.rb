9c2cefecdd314ad2c60b0f6e50e064f63a4f94d0
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 21:15:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.count >= 1
     current_line = "The line is currently: "
     katz_deli.each_with_index do |name, index|
-      puts current_line << " #{index + 1}. #{name}"
+      current_line << " #{index + 1}. #{name}"
     end
   else
     puts "The line is currently empty."
