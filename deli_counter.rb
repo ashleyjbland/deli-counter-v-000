fd78a27f255e1c59104039bd1e52b1c6965170fc
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 20:54:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ require 'pry'
 katz_deli = ["Logan", "Avi", "Spencer"]
 def line(katz_deli)
   if katz_deli.count >= 1
-    order_line: katz_deli.each_with_index do |name, index|
+    order_line = katz_deli.each_with_index do |name, index|
       print " #{index + 1}. #{name} "
     end
     order_line.insert(0, "The line is currently: ")
